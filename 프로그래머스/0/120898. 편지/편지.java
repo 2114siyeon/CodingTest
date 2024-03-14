77345404d8e8d949f03dba981ed8a2961d35854e
siyeon <103495064+2114siyeon@users.noreply.github.com>
2024-03-14 23:26:06 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        int eWidth = 2;
+        int latWidth;
+        int count = message.length();
+
+        latWidth = (eWidth*count);
+        answer = latWidth;
+        
+        
+        return answer;
+    }
+}
