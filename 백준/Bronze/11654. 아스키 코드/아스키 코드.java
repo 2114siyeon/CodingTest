1f338cebd3d246a27f480fb4570f20ab5bbac307
siyeon <103495064+2114siyeon@users.noreply.github.com>
2024-04-15 16:27:59 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.next().charAt(0);
+		
+		
+		
+		int i = (int)ch;
+		System.out.println(i);
+	}
+
+}
